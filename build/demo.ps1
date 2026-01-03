b6ef1220672196dc692b615fa9b227c885da1799
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-03 08:57:00 +0100

    chore(demo): refactor build scripts

--- build/demo.ps1
+++ build/demo.ps1
@@ -8,12 +8,15 @@ Remove-Item dist,$outconfig -Recurse -ErrorAction Ignore
 $watcher = & $PSScriptRoot/watcher $PWD
 [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
 Register-ObjectEvent -InputObject $watcher -EventName Changed -SourceIdentifier $id -Action {
-    if ($args[1].Name -ne $outconfig) { return }
-    Write-Host ([Environment]::NewLine + "<-- Restart -->") -ForegroundColor DarkMagenta
-    (rollup --config 2>&1) -join "`n" | Out-Host
+    if ($EventArgs.Name -ne $outconfig) { return }
+    [Environment]::NewLine + "<-- Restart -->"
+    (rollup --config 2>&1) -join "`n"
 } | Out-Null
 Push-Location ..
 [void] (babel $d/rollup.config.js --out-dir $d --out-file-extension .mjs --watch &)
 Pop-Location
 "The $outconfig watcher is ready."
-Wait-Event -SourceIdentifier $id
+while ($true) {
+    Wait-Event -SourceIdentifier $id -Timeout 5
+    Receive-Job -Name $id -ErrorAction SilentlyContinue
+}
