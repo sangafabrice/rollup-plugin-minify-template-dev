fd8a7ca91133d771e39fd17243a63c944f8852e6
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-04 09:56:39 +0100

    refactor(demo): complete most pwsh scripts to node conversion

---
+++ build/demo.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+import { loadConfigFile } from "rollup/loadConfigFile";
+import { rollup } from "rollup";
+import { watchFile, rmSync } from "node:fs";
+import { resolve } from "node:path";
+
+process.chdir("demo/");
+
+const cfgname = "rollup.config";
+const [ dist, config, configmjs ] = [
+        "dist",
+        ...[ ".js", ".mjs" ].map(ext => cfgname + ext) 
+    ];
+
+rmSync(dist, { force: true, recursive: true });
+
+watchFile(config, async function () {
+    if (!arguments.length)
+        console.log(`The ${config} watcher is ready.`);
+    await loadConfigFile(resolve(configmjs))
+        .then(({ options }) =>
+            options.forEach(async (option) => {
+                const { input, output } = option;
+                output.map((await rollup(option)).write);
+                console.info("✔", input, "→", output.map(({ file }) => file));
+            })
+        );
+}).emit("change");
--- demo/rollup.config.mjs
+++ demo/rollup.config.mjs
@@ -1,14 +1,12 @@
 import { createRequire } from "node:module";
+import { join } from "node:path";
 
 const require = createRequire(import.meta.url);
 require("@babel/register")({
     only: [ /rollup\.config\.js$/ ],
     cache: false,
     babelrc: false,
-    presets: [ [
-        "@babel/preset-flow",
-        { "experimental_useHermesParser": true }
-    ] ]
+    configFile: join(import.meta.dirname, "../build/.babelrc")
 });
 
 export default require("./rollup.config").default;
