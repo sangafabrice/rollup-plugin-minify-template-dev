83c9cfadf24576c0908f7dc7582915d6943f588a
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-04 07:14:54 +0100

    refactor(demo): reorganize babel config file

---
+++ build/package.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import getpkg, { outpkgConfig, pkgConfig } from "./main.js";
+import { writeFileSync, watchFile } from "node:fs";
+
+watchFile(pkgConfig, function () {
+    if (!arguments.length)
+        console.log(`The ${pkgConfig} watcher is ready.`);
+    writeFileSync(
+        outpkgConfig,
+        JSON.stringify(
+            getpkg(),
+            (key, value) => {
+                if (key != "scripts") return key == "name"
+                    ? value.slice(0, - "src".length - 1)
+                    : value
+            },
+            2
+        )
+    );
+    console.log("✔ emitted " + outpkgConfig);
+}).emit("change");
--- demo/rollup.config.mjs
+++ demo/rollup.config.mjs
@@ -3,7 +3,12 @@ import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 require("@babel/register")({
     only: [ /rollup\.config\.js$/ ],
-    cache: false
+    cache: false,
+    babelrc: false,
+    presets: [ [
+        "@babel/preset-flow",
+        { "experimental_useHermesParser": true }
+    ] ]
 });
 
 export default require("./rollup.config").default;
