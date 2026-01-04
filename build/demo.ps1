045c8805bb7c86cd629ab28d7f13b1387750b144
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-04 11:53:09 +0100

    refactor(demo): remove watcher unintended repeat

--- build/demo.ps1
+++ build/demo.ps1
@@ -7,16 +7,17 @@ Set-Location $d/
 Remove-Item dist -Recurse -ErrorAction Ignore
 $watcher = & $PSScriptRoot/watcher $PWD
 [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
-$job = Register-ObjectEvent $watcher Changed $id {
-    if ($args[1].Name -ne $outconfig) { return }
-    [Environment]::NewLine + "<-- Restart -->"
-    (rollup --config 2>&1) -join "`n"
-}
-& $job.Module {
-    & ([scriptblock]::Create($job.Command)) $watcher @{ Name = $outconfig }; ""
+$global:filechanged = $true
+$null = Register-ObjectEvent $watcher Changed $id {
+    if ($EventArgs.Name -eq $outconfig) {
+        $global:filechanged = $true
+    }
 }
 "The $outconfig watcher is ready."
 while ($true) {
-    Wait-Event $id -Timeout 5
-    Receive-Job $job -ErrorAction SilentlyContinue
+    if (!$filechanged) { continue  }
+    Wait-Event -Timeout 5
+    [Environment]::NewLine + "<-- Restart -->"
+    (rollup --config 2>&1) -join "`n"
+    $global:filechanged = $false
 }
