44b189112a1f961e077fd169d71263b62b765fe6
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-09 16:17:09 +0100

    refactor(demo): modify the commit script

--- build/commit-lib.ps1
+++ build/commit-lib.ps1
@@ -1,5 +1,16 @@
 #!/usr/bin/env pwsh
 
-foreach ($dir in 'lib/','src/') {
-    git -C $dir commit-all $($args[0])
+[CmdletBinding(PositionalBinding=$false)]
+param(
+    [Parameter(Mandatory, ValueFromRemainingArguments)]
+    [ValidateNotNullOrEmpty()]
+    [string] $message,
+    [ValidateSet("lib", "src")]
+    [ValidateCount(0,1)]
+    [string[]] $only = @("lib", "src")
+)
+
+foreach ($dir in $only) {
+    git -C $dir add --all
+    git -C $dir commit --message $message
 }
