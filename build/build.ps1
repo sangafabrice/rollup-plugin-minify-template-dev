c6ef2517390eff3acb21d05282802a659598f011
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-11 06:12:34 +0100

    refactor(demo): simplify build script

--- build/build.ps1
+++ build/build.ps1
@@ -4,8 +4,12 @@ Remove-Item -Path lib/*
 Copy-Item -Path src/* -Destination lib/
 
 [Console]::OutputEncoding = [Text.UTF8Encoding]::UTF8
-$null = Start-ThreadJob { transform }
-$null = Start-ThreadJob { write-declaration }
-$null = Start-ThreadJob { write-package }
+@(
+    { transform }
+    { write-declaration }
+    { write-package }
+) | ForEach-Object -Process {
+    Start-ThreadJob $_
+} | Out-Null
 
 Receive-Job (Get-Job) -Wait -ErrorAction SilentlyContinue
