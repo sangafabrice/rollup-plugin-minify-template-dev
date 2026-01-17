95c2a57d0dd9af76e4dfe1201468b37ac6e851f9
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-17 21:29:58 +0100

    chore(demo): use spawn for git command

--- setup/dependencies.cjs
+++ setup/dependencies.cjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-const { execSync, exec } = require("child_process");
+const cp = require("child_process");
+const { execSync, spawnSync, spawn } = cp;
 
 module.paths.push(execute("npm root --global"));
 const ncu = require("npm-check-updates");
@@ -12,13 +13,19 @@ ncu.run({
 .then(console.log)
 .then(() => {
     process.chdir("src/");
-    execute("git branch --show-current").length ||
-    exec("git switch main");
-    exec("git config get branch.lib.merge", e => {
-        if (!e) return;
-        execSync("git branch -f lib origin/lib");
-        execSync("git worktree add ../lib");
-    });
+    const app = "git";
+    const gitSync = spawnSync.bind(cp, app);
+    const git = spawn.bind(cp, app);
+    const isOutNotEmpty = function () {
+        return gitSync(...arguments)
+            .stdout.toString().trim().length;
+    }
+    isOutNotEmpty(["branch", "--show-current"]) ||
+            git(["switch", "main"]);
+    if (isOutNotEmpty(["config", "get", "branch.lib.merge"]))
+        return;
+    gitSync(["branch", "-f", "lib", "origin/lib"]);
+    gitSync(["worktree", "add", "../lib"]);
 })
 .then(() => console.log("✔ library and source setup complete."));
 
