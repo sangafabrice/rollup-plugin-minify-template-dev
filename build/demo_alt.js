bfe6ea7288447eac5dabf2d999043d75f8d38569
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-17 23:00:56 +0100

    chore(demo): convert demo pwsh to js

---
+++ build/demo_alt.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { execSync } from "node:child_process";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+const { watch = false } = yargs(hideBin(process.argv))
+    .scriptName("demo")
+    .boolean("watch")
+    .strict()
+    .help()
+    .parse();
+
+process.chdir("demo/");
+execSync(
+    `babel-node ` +
+    `--config-file "${import.meta.dirname}/.babelrc" ` +
+    `${watch ? "--watch --watch-preserve-output":""} rollup.config.cjs`,
+    { stdio: "inherit" }
+);
--- build/demo_alt.ps1
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env pwsh
-
-[CmdletBinding()]
-param ([switch] $watch)
-
-Set-Location -Path demo/ &&
-babel-node --config-file "$PSScriptRoot/.babelrc" $($watch ? "--watch":$null) rollup.config.cjs
