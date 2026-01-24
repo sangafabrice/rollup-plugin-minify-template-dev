5f6448076458c082cf1e0aa0a94c1a6cc5a4a29e
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-24 08:20:03 +0100

    chore: use transfer in-place of addfile

--- build/dl_images.cmd
+++ build/dl_images.cmd
@@ -1,18 +1,16 @@
 #!/usr/bin/env -S cmd /q /c
 
-setlocal
+setlocal EnableDelayedExpansion
 set imgpath=demo\w3c\img&
 mkdir %imgpath% 2> nul
-cd %imgpath% 2> nul && call :download bitsadmin.exe
+cd %imgpath% 2> nul && call :download
 endlocal 
 goto :eof
 
 :download
-for /f "skip=4 tokens=3" %%i in ('bitsadmin /create /download myjob') do set myjob=%%i&
-set "myjob=%myjob:.=%"
+set files=&
 for /f "usebackq" %%i in ("%~dp0\images.conf") do (
-    bitsadmin /addfile %myjob% https://i.imgur.com/%%i.jpg "%cd%\%%i.jpg" | find "Added"
+    set files=!files! https://i.imgur.com/%%i.jpg "%cd%\%%i.jpg"& @rem
 )
-bitsadmin /resume %myjob% | find "Job"
-bitsadmin /setNotifyCmdLine %myjob% %~$PATH:1 "bitsadmin /complete %myjob%" > nul 2>&1
+bitsadmin /transfer myjob %files%
 exit /b
