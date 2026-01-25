355b6d3207effeb1aa5a76afdc66643b705bf7be
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-25 18:41:25 +0100

    chore: simplify completion task

--- build/dl_images.cmd
+++ build/dl_images.cmd
@@ -14,5 +14,7 @@ for /f "usebackq" %%i in ("%~dp0\images.conf") do (
     bitsadmin /addfile %myjob% https://i.imgur.com/%%i.jpg "%cd%\%%i.jpg" | find "Added"
 )
 bitsadmin /resume %myjob% | find "Job"
-start /d "%~dp0" /min dl_images_completion
+for /f %%i in ("bitsadmin.exe") do (
+    bitsadmin /setNotifyCmdLine %myjob% %%~$PATH:i "bitsadmin /complete %myjob%" > nul 2>&1
+)
 exit /b
