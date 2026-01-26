cc48b3b4cdc00ea5958ad0f9f324b1146ac58edf
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-26 19:33:41 +0100

    chore: download jsmin with bitsadmin

--- setup/npmc.ps1
+++ setup/npmc.ps1
@@ -2,13 +2,23 @@ $npmps1 = @(where.exe npm.ps1)[0]
 if ([string]::IsNullOrEmpty($npmps1)) { return }
 $job = Start-ThreadJob {
     $root = $Using:PSScriptRoot
-    curl --config "$root/jsmin.conf" --output "$root/jsmin.exe"
+    $guid = (
+            bitsadmin /create /download myjob | 
+            Select-Object -Last 1
+        ) -replace "^.+(\{.+\})\.",'$1'
+    $source = "https://api.github.com/repos/douglascrockford/JSMin/contents/jsmin.exe"
+    bitsadmin /setcustomheaders $guid accept:application/vnd.github.raw > $null
+    bitsadmin /transfer $guid $source "$root\jsmin.exe" | find "Transfer complete."
+    bitsadmin /complete $guid | find "Job completed."
 }
 function Global:Find-PackageJson ($path) {
     if (Test-Path ($packagejson = Join-Path $path package.json))
         { return $packagejson }
     if ($null -ne ($parent = (Get-Item $path).Directory))
         { Find-PackageJson $parent }
+}
+function Global:< ($file, $app) {
+    Get-Content $scriptsjson -Raw | & $app | Out-String
 }
 Set-PSBreakpoint -Script $npmps1 -Line 2 -Action {
     if (${Script:args}[0] -inotin @(
@@ -21,14 +31,12 @@ Set-PSBreakpoint -Script $npmps1 -Line 2 -Action {
     $packagejson = Find-PackageJson $PWD
     $scriptsjson = Join-Path (Get-Item $packagejson).Directory package.scripts.json
     if (-not (Test-Path $scriptsjson)) { return }
-    $scriptsObject = Get-Content $scriptsjson -Raw |
-        & "$PSScriptRoot/jsmin.exe" |
-        Out-String |
+    $scriptsObject = < $scriptsjson "$PSScriptRoot/jsmin.exe" |
         ConvertFrom-Json
     $packageObject = Get-Content $packagejson -Raw |
         ConvertFrom-Json
     $packageObject.scripts = $scriptsObject
-    $packageObject | ConvertTo-Json | Out-File $packagejson
+    $packageObject | ConvertTo-Json | Out-File $packagejson -NoNewline
 }
 Receive-Job $job -Wait -AutoRemoveJob
 Write-Host ✔ PS BreakPoint Set
