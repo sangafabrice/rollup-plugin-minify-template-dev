a99bd9b96b7939f5316af1c1a30a5f023e855181
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-27 10:34:13 +0100

    chore: improve -no-server argument

--- build/watch_photo_demo.ps1
+++ build/watch_photo_demo.ps1
@@ -1,10 +1,17 @@
 #!/usr/bin/env pwsh
 
+[CmdletBinding(DefaultParameterSetName="noserver")]
 param(
+    [Parameter(ParameterSetName="server")]
+    [switch] $server,
+    [Parameter(ParameterSetName="noserver")]
     [switch] ${no-server}
 )
 
-$noServerParam = ${no-server} ? "--configNoServer":$null
+$noServerParam = (
+    $PSCmdlet.ParameterSetName -eq "server" ?
+        (-not $server):${no-server}
+) ? "--configNoServer":$null
 
 Set-Location "demo/"
 rollup --config --watch --no-watch.clearScreen $noServerParam
