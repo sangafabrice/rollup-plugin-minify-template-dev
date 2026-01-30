a513bf40654cda16643f94013d3eb84498e55722
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2026-01-30 04:01:43 +0100

    refactor(demo): convert pretty script from pwsh to node

--- build/pretty.ps1
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env pwsh
-
-(prettier . --write) -replace "^`e\[90m(.+)`e.+$",'$1' |
-ForEach-Object -Parallel {
-    $path = ($_.Trim() -split " ")[0]
-    $content = (Get-Content -Path $path -Raw).Trim()
-    Set-Content -Path $path -Value $content -NoNewline
-}
